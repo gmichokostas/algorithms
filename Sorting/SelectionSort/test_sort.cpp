080357db7c1c3a144e17c750fb64beed9890a305
George Michokostas <michokostas@gmail.com>
2017-10-03 15:12:59 +0200

    add more test examples

--- Sorting/SelectionSort/test_sort.cpp
+++ Sorting/SelectionSort/test_sort.cpp
@@ -1,13 +1,22 @@
 #include "selection.hpp"
 #include "catch.hpp"
 
-using Catch::Matchers::Equals;
-
-TEST_CASE("is_sorted", "[is_sorted]") {
+TEST_CASE("is_sorted", "[is_sorted]")
+{
     Selection<int> selection;
     int arr[] = {3, 1, 2};
 
 
     REQUIRE_FALSE( selection.is_sorted(arr) );
+}
+
+TEST_CASE("sort", "[sort]")
+{
+    Selection<int> selection;
+    int arr[] = {3, 1, 2};
+
+    selection.sort(arr);
+
+    REQUIRE( selection.is_sorted(arr) == true );
 }
 
